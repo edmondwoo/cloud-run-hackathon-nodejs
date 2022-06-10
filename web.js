f9141365809a8410964f892a0311b8e5c0529fde
Edmond Woo <edmond524524@yahoo.com.hk>
2022-06-10 14:21:54 +0800

    v1

--- web.js
+++ web.js
@@ -1,17 +1,84 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('Let the battle begin!');
+app.get("/", function (req, res) {
+  res.send("Let the battle begin!");
 });
 
-app.post('/', function (req, res) {
+app.post("/", function (req, res) {
   console.log(req.body);
-  const moves = ['F', 'T', 'L', 'R'];
-  res.send(moves[Math.floor(Math.random() * moves.length)]);
+  const data = req.body;
+  //const moves = ["F", "T", "L", "R"];
+  const moves = ["F", "L", "R"];
+  if (checkHavePeopleToShot(data)) {
+    res.send(moves["T"]);
+  } else {
+    res.send(moves[Math.floor(Math.random() * moves.length)]);
+  }
 });
 
 app.listen(process.env.PORT || 8080);
+
+function getMyLink(data) {
+  return data._links.self.href;
+}
+function getMyState(data) {
+  const link = getMyLink(data);
+  const state = link;
+  console.log(state);
+
+  return state;
+}
+function checkHavePeopleToShot(data) {
+  const state = getMyState(data);
+  const friends = data.arena.state;
+  const myX = state.x;
+  const myY = state.y;
+  switch (state.direction) {
+    case "N":
+      friends.forEach(function (friend) {
+        console.log(friend);
+        if (myX === friend.x) {
+          if (myY - friend.y >= 0 && myY - friend.y <= 3) {
+            return true;
+          }
+        }
+      });
+      break;
+    case "W":
+      friends.forEach(function (friend) {
+        console.log(friend);
+        if (myY === friend.y) {
+          if (myX - friend.x >= 0 && myX - friend.x <= 3) {
+            return true;
+          }
+        }
+      });
+      break;
+    case "S":
+      friends.forEach(function (friend) {
+        console.log(friend);
+        if (myY === friend.y) {
+          if (myX - friend.x <= 0 && myX - friend.x >= -3) {
+            return true;
+          }
+        }
+      });
+      break;
+    case "E":
+      friends.forEach(function (friend) {
+        console.log(friend);
+        if (myX === friend.x) {
+          if (myY - friend.y <= 0 && myX - friend.x >= -3) {
+            return true;
+          }
+        }
+      });
+      break;
+    default:
+      return false;
+  }
+}
